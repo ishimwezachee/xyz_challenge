b016a136dfd346199e3b3537f0b86fa57d0ee892
zachee <zacheeishimwe@gmail.com>
2021-05-31 18:41:48 +0200

    push the codes

---
+++ src/components/home/home.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import Nav from '../navigation/nav';
+import image from "../download.jpeg";
+import "./home.css";
+import Card from '../../components/cards/card'
+export default function home() {
+    return (
+        <div className="home">
+            <Nav/>
+            <div className="imgText">
+             <div className="imag">
+                <img  className="image" src={image}/>
+             </div>
+            <div className="text-button">
+            <div className="texts">
+                 <h2>
+                     Empower Leads
+                 </h2>
+                 <h2>
+                     and
+                 </h2>
+                 <h2>
+                     Change Makers 
+                 </h2>
+             </div>
+             <div className="buttons">
+               <button id="btn1">Become a Montlty contributor</button>
+               <button id="btn2">Make a one time contribution</button>
+             </div>
+            </div>
+             </div>
+             <div className="text">
+               <h1>Let's build this Foundation together.</h1>
+               <p className="lowerText">
+               Become a monthly contributor and keep an eye on your mailbox
+                 for a token of appreciation sent straight from the Obama
+                Foundation’s Chicago HQ. Choose your contribution.  
+               </p>
+             </div>
+             <div className="cards">
+             <Card/>
+             </div>
+        </div>
+    )
+}
+
+
---
+++ src/components/navigation/nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./nav.css"
+export default function home() {
+    return (
+        <div className="nav">
+            <div>
+            <p className="top-words"><strong>OBAMA</strong>Donate</p>
+            </div>
+            <div>
+            <button className="top">Donate Now</button>
+            </div>
+        </div>
+    )
+}
+
+
